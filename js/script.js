17f5796ca1555f7a6a96adbe10c0ef51702e1668
basel-aljeshi <basel.aljeshi@gmail.com>
2024-08-28 21:40:06 -0400

    added brushstrokes and skills to project section

--- js/script.js
+++ js/script.js
@@ -1,49 +1,60 @@
-function toggleMenu(){
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-
-}
-
 // js/script.js
 
 document.addEventListener('DOMContentLoaded', () => {
+  const skillBrushstrokes = {
+    'Embedded Programming': './assets/brushstrokes/1x/indigo.png',
+    'Java': './assets/brushstrokes/1x/light-blue.png',
+    'Python': './assets/brushstrokes/1x/purple.png',
+    'Tableau': './assets/brushstrokes/1x/orange.png',
+    '3D Printing': './assets/brushstrokes/1x/yellow.png',
+    'HTML/CSS': './assets/brushstrokes/1x/dark-green.png',
+    'Javascript': './assets/brushstrokes/1x/royal-blue.png',
+    'MATLAB': './assets/brushstrokes/1x/green.png',
+    'SQL': './assets/brushstrokes/1x/gold.png',
+    'Solidworks': './assets/brushstrokes/1x/red.png',
+    'Figma': './assets/brushstrokes/1x/pink.png'
+  };
+
   const projects = [
     {
       title: 'Project One',
       imgSrc: './assets/pic/projects-1.png',
       githubLink: 'https://github.com',
-      liveDemoLink: 'https://github.com'
+      liveDemoLink: 'https://github.com',
+      skills: ['Solidworks', 'Figma', 'Python']
     },
     {
       title: 'Project Two',
       imgSrc: './assets/pic/projects-2.png',
       githubLink: 'https://github.com',
-      liveDemoLink: 'https://github.com'
+      liveDemoLink: 'https://github.com',
+      skills: ['Tableau', 'Javascript', 'SQL']
     },
     {
       title: 'Project Three',
       imgSrc: './assets/pic/projects-3.png',
       githubLink: 'https://github.com',
-      liveDemoLink: 'https://github.com'
+      liveDemoLink: 'https://github.com',
+      skills: ['Python', '3D Printing', 'Embedded Programming']
     },
     {
       title: 'Project Four',
       imgSrc: './assets/pic/projects-4.png',
       githubLink: 'https://github.com',
-      liveDemoLink: 'https://github.com'
+      liveDemoLink: 'https://github.com',
+      skills: ['Solidworks', 'Tableau', 'SQL']
     },
     {
       title: 'Project Five',
       imgSrc: './assets/pic/projects-5.png',
       githubLink: 'https://github.com',
-      liveDemoLink: 'https://github.com'
+      liveDemoLink: 'https://github.com',
+      skills: ['Java', 'Python', 'SQL']
     }
   ];
 
   const projectsContainer = document.querySelector('.projects-container');
+  projectsContainer.innerHTML = ''; // Clear any existing content
 
   projects.forEach(project => {
     const projectCard = document.createElement('div');
@@ -56,6 +67,9 @@ document.addEventListener('DOMContentLoaded', () => {
       <div class="btn-container">
         <button class="btn btn-color-2 project-btn" onclick="location.href='${project.githubLink}'">Github</button>
         <button class="btn btn-color-2 project-btn" onclick="location.href='${project.liveDemoLink}'">Live Demo</button>
+      </div>
+      <div class="skills-container">
+        ${project.skills.map(skill => `<div class="skill" style="background-image: url('${skillBrushstrokes[skill]}')">${skill}</div>`).join('')}
       </div>
     `;
     projectsContainer.appendChild(projectCard);
